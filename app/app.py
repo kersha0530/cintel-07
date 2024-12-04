4388f59f0ea734e1a2eab1019725ad1a67221f14
kersha0530 <168761614+kersha0530@users.noreply.github.com>
2024-12-03 18:56:33 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,79 +1,76 @@
+# Define imports
 import seaborn as sns
 from faicons import icon_svg
-
 from shiny import reactive
 from shiny.express import input, render, ui
-import palmerpenguins 
+import palmerpenguins
+from shinyswatch import theme  # Import shinyswatch to use bootswatch themes
 
+# Define Dataframe
 df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Penguins dashboard", fillable=True)
+# Page Title with Cyborg theme
+ui.page_opts(title="Penguins dashboard", fillable=True, theme=theme.cyborg)
 
-
+# Define Sidebar title
 with ui.sidebar(title="Filter controls"):
+
+    # Input slider to define penguin mass
     ui.input_slider("mass", "Mass", 2000, 6000, 6000)
+
+    # Input checkbox to select which species to display in outputs
     ui.input_checkbox_group(
         "species",
         "Species",
         ["Adelie", "Gentoo", "Chinstrap"],
         selected=["Adelie", "Gentoo", "Chinstrap"],
     )
+
+    # Horizontal rule for separating sections
     ui.hr()
+
+    # Helpful links
     ui.h6("Links")
-    ui.a(
-        "GitHub Source",
-        href="https://github.com/denisecase/cintel-07-tdash",
-        target="_blank",
-    )
-    ui.a(
-        "GitHub App",
-        href="https://denisecase.github.io/cintel-07-tdash/",
-        target="_blank",
-    )
-    ui.a(
-        "GitHub Issues",
-        href="https://github.com/denisecase/cintel-07-tdash/issues",
-        target="_blank",
-    )
+    ui.a("GitHub Source", href="https://github.com/kersha0530/cintel-07-tdash", target="_blank")
+    ui.a("GitHub Issues", href="https://github.com/kersha0530/cintel-07-tdash/issues", target="_blank")
     ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
-    ui.a(
-        "Template: Basic Dashboard",
-        href="https://shiny.posit.co/py/templates/dashboard/",
-        target="_blank",
-    )
-    ui.a(
-        "See also",
-        href="https://github.com/denisecase/pyshiny-penguins-dashboard-express",
-        target="_blank",
-    )
+    ui.a("Template: Basic Dashboard", href="https://shiny.posit.co/py/templates/dashboard/", target="_blank")
 
-
+# Main page content
 with ui.layout_column_wrap(fill=False):
-    with ui.value_box(showcase=icon_svg("earlybirds")):
+
+    # Output of number of penguins based on species selected
+    with ui.value_box(showcase=icon_svg("earlybirds"), style="color:#56cc9d;"):
         "Number of penguins"
 
         @render.text
         def count():
             return filtered_df().shape[0]
 
-    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
+    # Output of average bill length
+    with ui.value_box(showcase=icon_svg("ruler-horizontal"), style="color: #fd7e14;"):
         "Average bill length"
 
         @render.text
         def bill_length():
             return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
 
-    with ui.value_box(showcase=icon_svg("ruler-vertical")):
+    # Output of average bill depth
+    with ui.value_box(showcase=icon_svg("ruler-vertical"), style="color: #007bff;"):
         "Average bill depth"
 
         @render.text
         def bill_depth():
             return f"{filtered_df()['bill_depth_mm'].mean():.1f} mm"
 
-
 with ui.layout_columns():
+
+    # Bill Length and Depth scatterplot based on species selected
     with ui.card(full_screen=True):
-        ui.card_header("Bill length and depth")
+        ui.card_header(
+            "Bill length and depth",
+            style="font-weight: bold; color: #56cc9d;",
+        )
 
         @render.plot
         def length_depth():
@@ -84,8 +81,12 @@ with ui.layout_columns():
                 hue="species",
             )
 
+    # Filtered DataGrid based on selected species
     with ui.card(full_screen=True):
-        ui.card_header("Penguin Data")
+        ui.card_header(
+            "Penguin data",
+            style="font-weight: bold; color: #56cc9d;",
+        )
 
         @render.data_frame
         def summary_statistics():
@@ -98,12 +99,10 @@ with ui.layout_columns():
             ]
             return render.DataGrid(filtered_df()[cols], filters=True)
 
-
-#ui.include_css(app_dir / "styles.css")
-
-
+# Define reactive calc for filtered df
 @reactive.calc
 def filtered_df():
     filt_df = df[df["species"].isin(input.species())]
     filt_df = filt_df.loc[filt_df["body_mass_g"] < input.mass()]
     return filt_df
+
